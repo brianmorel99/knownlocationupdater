f09065b50ce1397cf49aad37dcb4db0639d579b3
Brian Morel <brianmorel99@gmail.com>
2025-02-25 15:09:26 -0500

    Start on Website to update values.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import uvicorn
 import base64
 from graph import check_named_location
 from app_config import read_config, write_config
-from fastapi import FastAPI, Request, status
-from fastapi.responses import Response, JSONResponse
+from fastapi import FastAPI, Request, status, Form
+from fastapi.responses import Response, JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -28,7 +28,7 @@ async def check_authentication(request: Request, username: str, password: str):
 
 @app.get("/")
 async def catch_all(request: Request, hostname: str = "", myip: str = ""):
-    
+
     if not await check_authentication(request, ddns_username, ddns_password):
         return JSONResponse(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -57,6 +57,7 @@ async def catch_all(request: Request, hostname: str = "", myip: str = ""):
             content="Invalid Data"
         )
 
+
 @app.get("/admin")
 async def administration(request: Request):
     if not await check_authentication(request, admin_username, admin_password):
@@ -66,10 +67,38 @@ async def administration(request: Request):
             content="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-    
+
     config = read_config()
-    print(config)
     return templates.TemplateResponse(request=request, name="locations.html", context={"configs": config})
+
+
+@app.post("/admin")
+async def admin_post(request: Request,
+                     location_id: str = Form(),
+                     display_name: str = Form(),
+                     ip_address: str = Form(),
+                     is_trusted: bool = Form(False),
+                     client_id: str = Form(),
+                     client_secret: str = Form(),
+                     tenant_id: str = Form()
+                     ):
+    
+    if not await check_authentication(request, admin_username, admin_password):
+
+        return Response(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    print(f"Location ID: %s", location_id)
+    print(f"Display Name: %s", display_name)
+    print(f"IP Address: %s", ip_address)
+    print(f"Is Trusted: %s", is_trusted)
+    print(f"Client ID: %s", client_id)
+    print(f"Client_Secret: %s", client_secret)
+    print(f"Tenant ID: %s", tenant_id)
+
+    return RedirectResponse(url='/admin', status_code=302)
 
 async def main():
     config = uvicorn.Config("main:app", port=8080, log_level="info")
